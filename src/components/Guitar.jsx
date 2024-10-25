a349c6587d18e173431966919830918e1bb63627
Jose Pablo Navarro <jnavarror@ucenfotec.ac.cr>
2024-10-24 22:13:12 -0600

    Este es un commit didactico

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -3,7 +3,7 @@ export default function Guitar({ guitar, addToCart }) {
 
     // eslint-disable-next-line react/prop-types
     const { id, name, image, description, price } = guitar;
-
+    //comentario
     return (
         <div className="col-md-6 col-lg-4 my-4 row align-items-center">
             <div className="col-4">
